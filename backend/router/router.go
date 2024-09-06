e74b6dfbb0feb436481436e138bf79698f128d35
Tahcin Ul Karim Mycin <150533238+mmycin@users.noreply.github.com>
2024-09-06 15:33:51 +0600

    Update router.go

--- backend/router/router.go
+++ backend/router/router.go
@@ -9,10 +9,10 @@ import (
 func Router() *gin.Engine {
 	r := gin.Default()
 	config := cors.DefaultConfig()
-    config.AllowAllOrigins = true
-    config.AllowMethods = []string{"POST", "GET", "PUT", "OPTIONS"}
-    config.AllowHeaders = []string{"Origin", "Content-Type", "Authorization", "Accept", "User-Agent", "Cache-Control", "Pragma"}
-    config.ExposeHeaders = []string{"Content-Length"}
+    	config.AllowAllOrigins = true
+    	config.AllowMethods = []string{"POST", "GET", "PUT", "OPTIONS"}
+    	config.AllowHeaders = []string{"Origin", "Content-Type", "Authorization", "Accept", "User-Agent", "Cache-Control", "Pragma"}
+    	config.ExposeHeaders = []string{"Content-Length"}
 	r.Use(cors.New(config))
     
 	r.GET("/notice", controller.GetAllNotice)
